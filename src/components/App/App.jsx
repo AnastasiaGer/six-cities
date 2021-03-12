53f4a7f48b7c10d6387bcc40062645a0b99ce81f
AnastasiaGer <aagermanova@gmail.com>
2021-03-11 17:24:45 -0800

    module2-task1 Card

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Main from '../Main/Main.jsx';
 
 const App = (props) => {
-  const {housingCount} = props;
+  const {housingCount, namesCards} = props;
 
   return (
     <Main
       housingCount={housingCount}
+      namesCards={namesCards}
     />
   );
 };
+
+App.propTypes ={
+  housingCount:PropTypes.number.isRequired,
+  namesCards: PropTypes.arrayOf(PropTypes.string.isRequired)
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import App from './components/app/app.jsx';
 
 
 const Ads = {
-  HOUSING_COUNT: 312
+  HOUSING_COUNT: 312,
 };
+
+const namesCards = [`Beautiful &amp; luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`, `Wood and stone place`];
+
 
 ReactDOM.render(
     <App
       housingCount={Ads.HOUSING_COUNT}
+      namesCards={namesCards}
     />,
     document.querySelector(`#root`)
 );
